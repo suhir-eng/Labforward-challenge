cd9b56165f2861f51c1ecf84885f49debacb4e26
suhir-eng <soheir-bt@hotmail.com>
2020-07-14 20:49:47 +0200

    existing endpoint updated

--- src/main/java/com/labforward/api/hello/controller/HelloController.java
+++ src/main/java/com/labforward/api/hello/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.labforward.api.hello.controller;
 
+import com.labforward.api.core.creation.EntityCreatedResponse;
 import com.labforward.api.core.exception.ResourceNotFoundException;
 import com.labforward.api.hello.domain.Greeting;
 import com.labforward.api.hello.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 @RestController
 public class HelloController {
 
 	public static final String GREETING_NOT_FOUND = "Greeting Not Found";
 
 	private HelloWorldService helloWorldService;
-
+    @Autowired
 	public HelloController(HelloWorldService helloWorldService) {
 		this.helloWorldService = helloWorldService;
 	}
@@ -35,7 +39,16 @@ public class HelloController {
 	}
 
 	@RequestMapping(value = "/hello", method = RequestMethod.POST)
-	public Greeting createGreeting(@RequestBody Greeting request) {
-		return helloWorldService.createGreeting(request);
+	public EntityCreatedResponse createGreeting(@RequestBody Greeting request) throws URISyntaxException {
+		Greeting greeting = helloWorldService.createGreeting(request);
+		return new EntityCreatedResponse<>(greeting, new URI("/hello/".concat(greeting.getId())));
+
+	}
+
+	@RequestMapping(value = "/hello/{id}", method = RequestMethod.PATCH)
+	@ResponseBody
+	public Greeting updateGreeting(@PathVariable String id, @RequestBody Greeting request) {
+		return helloWorldService.updateGreeting(id, request)
+				.orElseThrow(() -> new ResourceNotFoundException(GREETING_NOT_FOUND));
 	}
 }
